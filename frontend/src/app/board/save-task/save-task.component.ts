c1d5ec51f7ad3f511fc691d465fe4e5c326bc1d2
Juan David <juanda7473@gmail.com>
2021-08-31 15:28:59 -0500

    update

--- frontend/src/app/board/save-task/save-task.component.ts
+++ frontend/src/app/board/save-task/save-task.component.ts
@@ -19,6 +19,7 @@ export class SaveTaskComponent implements OnInit {
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
   durationInSeconds: number = 2;
+  selectedFile: any;
 
   constructor(
     private _boardService: BoardService,
@@ -27,6 +28,7 @@ export class SaveTaskComponent implements OnInit {
   ) {
     this.message = '';
     this.registerData = {};
+    this.selectedFile = null;
   }
 
   ngOnInit(): void {}
@@ -39,6 +41,41 @@ export class SaveTaskComponent implements OnInit {
       this.registerData = {};
     } else {
       this._boardService.saveTask(this.registerData).subscribe(
+        (res) => {
+          console.log(res);
+          //despues de que se registre lo redireccione
+          this._router.navigate(['/listTaks']);
+          this.message = 'Task create';
+          this.openSnackBarSuccesfull();
+          //limpio el json
+          this.registerData = {};
+        },
+        (err) => {
+          console.log(err);
+          this.message = err.error;
+          this.openSnackBarError();
+        }
+      );
+    }
+  }
+
+  uploadImg(event: any) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  saveTaskImg() {
+    if (!this.registerData.name || !this.registerData.description) {
+      console.log('Failed process: Incomplete data');
+      this.message = 'Failed process: Incomplete data';
+      this.openSnackBarError();
+      this.registerData = {};
+    } else {
+      const data = new FormData();
+      data.append('image', this.selectedFile, this.selectedFile.name);
+      data.append('name', this.registerData.name);
+      data.append('description', this.registerData.description);
+
+      this._boardService.saveTaskImg(data).subscribe(
         (res) => {
           console.log(res);
           //despues de que se registre lo redireccione
